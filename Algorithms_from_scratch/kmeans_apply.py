b395f7dfe53c66f6ae61a13fdae2bd8709c56211
shyamgupta196 <60971003+shyamgupta196@users.noreply.github.com>
2020-11-20 15:12:22 +0530

    full kmeans apply

--- Algorithms_from_scratch/kmeans_apply.py
+++ Algorithms_from_scratch/kmeans_apply.py
@@ -1,21 +1,20 @@
 import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
+df = pd.read_csv('heart.csv')
+model = KMeans(4,max_iter=50)
+df.age = np.array(df.age).reshape(-1,1)
+print(df.age.shape)
+preds = model.fit_predict(df.age)
+print(model)
 
+# model.predict(df.age)
 
+print(model.cluster_centers_)
 
+df['preds'] = preds
 
-
-
-
-
-
-
-
-
-
-
-# model = KMeans(4,max_iter=50)
-# df.age = np.array(df.age).reshape(-1,1)
-# print(df.age.shape)
-# model.fit(df.age)
-# print(model)
+plt.scatter(df['preds'],df['age'],c=df['preds'])
+plt.show()
